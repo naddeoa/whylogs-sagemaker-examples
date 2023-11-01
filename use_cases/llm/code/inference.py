f3912091243e91b6cfa6c606f25e31632f30f1d9
Anthony Naddeo <anthony@whylabs.ai>
2023-11-01 11:56:14 -0700

    Update llm example
    The langkit version was really old

--- use_cases/llm/code/inference.py
+++ use_cases/llm/code/inference.py
@@ -1,33 +1,33 @@
 import traceback
-import torch
-import requests
 import json
 import os
+import logging
 
 import whylogs as why
-from whylogs.api.logger.experimental.logger.actor.process_rolling_logger import ProcessRollingLogger
+from whylogs.api.writer import Writer, Writers
+from whylogs.api.logger.experimental.logger.actor.thread_rolling_logger import ThreadRollingLogger
 from langkit import llm_metrics # alternatively use 'light_metrics'
 from whylogs.api.logger.experimental.logger.actor.time_util import Schedule, TimeGranularity
 from transformers import GPT2Tokenizer, TextGenerationPipeline, GPT2LMHeadModel
 
 
+logging.basicConfig(level=logging.DEBUG)
+
 # Initialize whylogs with your WhyLabs API key and target dataset ID. You can get an api key from the
-# settings menu of you WhyLabs account. This loads credentials from the env directly
-why.init()
-# why.init(whylabs_api_key="KYE3amul9h.vKnLtOVp1IPcyAv7lEbgLEhGUrNirJ0RYEXVUhB6ZgSBHgaF7SbuF:org-JpsdM6", default_dataset_id="model-86")  # TODO delete
-row_name = "image"
+# settings menu of you WhyLabs account. 
+why.init() # This loads credentials from the env directly
 
 def create_logger():
-    logger = ProcessRollingLogger(
+    logger = ThreadRollingLogger(
         # This should match the model type in WhyLabs. We're using a daily model here.
         aggregate_by=TimeGranularity.Day,
         # The profiles will be uploaded from the rolling logger to WhyLabs every 5 minutes. Data
         # will accumulates during that time.
         write_schedule=Schedule(cadence=TimeGranularity.Minute, interval=5),
+        writers=[Writers.get('whylabs')],
         schema=llm_metrics.init(),
     )
 
-    logger.start()
     return logger
 
 
@@ -53,13 +53,6 @@ def model_fn(model_dir):
     return TextGenerationPipeline(model=model, tokenizer=tokenizer)
 
 
-def predict_fn(input_data, model):
-    """
-    Apply model to the incoming request
-    """
-
-    return model.__call__(input_data)
-
 def input_fn(request_body, request_content_type):
     assert request_content_type == 'application/json'
     return json.loads(request_body)
@@ -77,11 +70,13 @@ def predict_fn(input_data, model):
         return 'closed'
 
     output = model.__call__(input_data, max_length=100)
+    output = output[0]['generated_text']
 
     try:
         # Log image async with whylogs. This won't hold up predictions.
-        print(f'>> prompt: ${input_data}')
-        logger.log({'prompt': input_data, 'response': output})  
+        row = {'prompt': input_data, 'response': output}
+        print(f'Row: {row}')
+        logger.log(row)
     except Exception as e:
         print(f"Failed to log image: {e}")
         print(traceback.format_exc())
@@ -90,3 +85,4 @@ def predict_fn(input_data, model):
 
 def output_fn(prediction, content_type):
     return str(prediction)
+
